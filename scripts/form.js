939eb20c712e0f08149134b914d3b410f4443820
jdethington <jdethington@gmail.com>
2026-04-06 14:57:14 -0700

    week05

--- scripts/form.js
+++ scripts/form.js
@@ -38,16 +38,3 @@ products.forEach(product => {
     productSelect.appendChild(option);
 })
 
-// ------ review.html -----
-function updateReviewCounter() {
-    let count = localStorage.getItem('totalReviews');
-    if (count === null) {
-        count = 0;
-    } else {
-        count = parseInt((count));
-    }
-    count++;
-    localStorage.setItem('totalReviews', count);
-    document.getElementById('reviewCount').textContent = count;
-}
-updateReviewCounter();
