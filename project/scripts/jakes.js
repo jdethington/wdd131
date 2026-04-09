bf659101599c8fb64ce9a54a6dc4d3ed94ee869d
jdethington <jdethington@gmail.com>
2026-04-09 09:15:37 -0700

    project day one

---
+++ project/scripts/jakes.js
@@ -0,0 +1,11 @@
+document.getElementById("lastModified").innerHTML = document.lastModified;
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
