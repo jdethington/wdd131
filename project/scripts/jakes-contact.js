28633770fa4c6cb0564cd20ebb09b624031e4e82
jdethington <jdethington@gmail.com>
2026-04-13 15:04:01 -0700

    Project Contact forms look good

--- project/scripts/jakes-contact.js
+++ project/scripts/jakes-contact.js
@@ -1,14 +1,14 @@
-// footer content
-document.getElementById("lastModified").innerHTML = document.lastModified;
-document.getElementById('currentYear').textContent = new Date().getFullYear();
-// Menu toggle
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+// // footer content
+// document.getElementById("lastModified").innerHTML = document.lastModified;
+// document.getElementById('currentYear').textContent = new Date().getFullYear();
+// // Menu toggle
+// const hamButton = document.querySelector('#menu');
+// const navigation = document.querySelector('.navigation');
 
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
+// hamButton.addEventListener('click', () => {
+//     navigation.classList.toggle('open');
+//     hamButton.classList.toggle('open');
+// });
 
 // form validation
 const products = [
