322bca8c05968f17cacc3d877330311798217e2e
jdethington <jdethington@gmail.com>
2026-04-13 20:17:22 -0700

    .

--- project/scripts/jakes-index.js
+++ project/scripts/jakes-index.js
@@ -1,14 +1,4 @@
-// // footer content
-// document.getElementById("lastModified").innerHTML = document.lastModified;
-// document.getElementById('currentYear').textContent = new Date().getFullYear();
-// // Menu toggle
-// const hamButton = document.querySelector('#menu');
-// const navigation = document.querySelector('.navigation');
 
-// hamButton.addEventListener('click', () => {
-//     navigation.classList.toggle('open');
-//     hamButton.classList.toggle('open');
-// });
 // Cheese and cow data
 const cheeses = [
     {
