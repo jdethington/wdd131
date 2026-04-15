6a90fae977913ba581df00b1929f8ee6d3a1441c
jdethington <jdethington@gmail.com>
2026-04-14 21:30:06 -0700

    fun times

--- week02/scripts/debugging.js
+++ week02/scripts/debugging.js
@@ -13,3 +13,20 @@ radius = 20;
 area = PI * radius * radius;
 radiusOutput.textContent = radius;
 areaOutput.textContent = area;
+
+// Example code for debugging practice. This code contains several errors that need to be fixed in order for it to work correctly.
+// const radiusOutput = document.getElementById('radius');
+// const areaOutput = document.querySelector('area');
+
+// let area = 0;
+// const PI == 3.14159;
+
+// const radius = 10;
+// area = PI * radius * radius;
+// radiusOutput = radius;
+// areaOutput = area;
+
+// radius = 20;
+// area = PI * radius * radius;
+// radiusOutput = radius;
+// areaOutput = area;
